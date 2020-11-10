d9b587b6b976fe431aab03c9d3a2bc7463754548
Marc Dagatan <marc.dagatan@gmail.com>
2020-11-10 23:11:10 +0800

    Add fetchingAPokemon
    Add fetching state for both single and multiple pokemon

--- src/actions/pokedex_actions.js
+++ src/actions/pokedex_actions.js
@@ -1,7 +1,9 @@
-import { FETCH_POKEMON } from './types';
+import { FETCH_POKEMON, FETCHING_POKEMON, FETCH_A_POKEMON, FETCHING_A_POKEMON } from './types';
 import axios from '../config/axios';
 
 export const fetchPokemon = (limit = 100, offset = 0) => async dispatch => {
+  dispatch({ type: FETCHING_POKEMON, payload: true });
+
   try {
     let {
       data: { results: payload },
@@ -9,10 +11,27 @@ export const fetchPokemon = (limit = 100, offset = 0) => async dispatch => {
     } = await axios.get(`/pokemon?limit=${limit}&offset=${offset}`);
 
     if (status === 200) dispatch({ type: FETCH_POKEMON, payload });
+
+    dispatch({ type: FETCHING_POKEMON, payload: false });
   } catch (error) {
-    // when you have time, add a notification snackbar
     Promise.reject(error);
+
+    dispatch({ type: FETCHING_POKEMON, payload: false });
   }
 };
 
-export const placeholder = () => {};
+export const fetchAPokemon = id => async dispatch => {
+  dispatch({ type: FETCHING_A_POKEMON, payload: true });
+
+  try {
+    let { data: payload, status } = await axios.get(`/pokemon/${id}`);
+
+    if (status === 200) dispatch({ type: FETCH_A_POKEMON, payload });
+
+    dispatch({ type: FETCHING_A_POKEMON, payload: false });
+  } catch (error) {
+    Promise.reject(error);
+
+    dispatch({ type: FETCHING_A_POKEMON, payload: false });
+  }
+};
--- src/actions/types.js
+++ src/actions/types.js
@@ -1,2 +1,5 @@
 export const FETCH_POKEMON = 'fetch_pokemon';
+export const FETCHING_POKEMON = 'fetching_pokemon';
+export const FETCH_A_POKEMON = 'fetch_a_pokemon';
+export const FETCHING_A_POKEMON = 'fetching_a_pokemon';
 export const SELECT_POKEMON = 'select_pokemon';
