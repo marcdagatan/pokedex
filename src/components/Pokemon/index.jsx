6a12aefe80bfe19e2765c3a23658d217d60a43a2
Marc Dagatan <marc.dagatan@gmail.com>
2020-11-11 03:28:51 +0800

    Add clear focused pokemon, with button
    Refactor action names

--- src/components/Pokemon/index.jsx
+++ src/components/Pokemon/index.jsx
@@ -1,17 +1,36 @@
 import React from 'react';
-import { pokemonPropTypes } from '../../propTypes';
+import styled from 'styled-components';
+import PropTypes from 'prop-types';
+import { Button } from '@material-ui/core';
 
+import { pokemonPropTypes } from '../../propTypes';
 import { HeroImage, Stats } from './components';
 
-const Pokemon = ({ pokemon: { stats, ...pokemon } }) => (
-  <>
+const Container = styled.div`
+  position: relative;
+`;
+
+const ClearContainer = styled.div`
+  position: absolute;
+  top: 0;
+  right: 1rem;
+`;
+
+const Pokemon = ({ pokemon: { stats, ...pokemon }, clearFocusedPokemon }) => (
+  <Container>
+    <ClearContainer>
+      <Button onClick={clearFocusedPokemon} color="primary" variant="outlined">
+        Clear
+      </Button>
+    </ClearContainer>
     <HeroImage url={pokemon.sprites.frontDefault} />
     <h2>{pokemon.name}</h2>
     <Stats stats={stats} />
-  </>
+  </Container>
 );
 Pokemon.propTypes = {
   pokemon: pokemonPropTypes.isRequired,
+  clearFocusedPokemon: PropTypes.func.isRequired,
 };
 
 export default Pokemon;
