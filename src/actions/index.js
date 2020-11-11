aec25ce3c0bcab7c18d06c687ec3dcf6dba9923d
Marc Dagatan <marc.dagatan@gmail.com>
2020-11-11 23:30:00 +0800

    Add action and reducer response to select and add pokemon

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,2 @@
 export * from './pokedex_actions';
+export * from './my_pokemon_actions';
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,4 +1,5 @@
 import { combineReducers } from 'redux';
 import pokedex from './pokedex_reducer';
+import myPokemon from './my_pokemon_reducer';
 
-export default combineReducers({ pokedex });
+export default combineReducers({ pokedex, myPokemon });
