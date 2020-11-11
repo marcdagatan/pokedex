0e29ff7ce72642ad95c630c202ea6129fb1c7c9b
Marc Dagatan <marc.dagatan@gmail.com>
2020-11-11 23:30:44 +0800

    Add PokeHeader component

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 export { default as Pokedex } from './Pokedex';
 export { default as PokedexList } from './PokedexList';
 export { default as PokedexListItem } from './PokedexListItem';
+export { default as PokeHeader } from './PokeHeader';
