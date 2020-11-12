0624577e8d7ac678a7e22b1c6061435cba16a211
Marc Dagatan <marc.dagatan@gmail.com>
2020-11-12 17:15:50 +0800

    Add boilerplate for customizations component

---
+++ src/components/Customizations/index.jsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Customizations = () => <h1>Customizations</h1>;
+
+export default Customizations;
---
+++ src/containers/CustomizationsContainer.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Customizations } from '../components';
+
+const CustomizationsContainer = () => <Customizations />;
+
+export default CustomizationsContainer;
