e99a31bc7db624dd3dfb98f2384144177c4d43c5
Marc Dagatan <marc.dagatan@gmail.com>
2020-11-12 17:08:01 +0800

    Update action type name SET_POKEMON

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,4 +1,4 @@
-export const SET_POKEMON = 'SET_POKEMON';
+export const SET_POKEMON = 'set_pokemon';
 export const FETCHING_POKEMON = 'fetching_pokemon';
 export const SET_FOCUSED_POKEMON = 'set_focused_pokemon';
 export const FETCHING_A_POKEMON = 'fetching_a_pokemon';
